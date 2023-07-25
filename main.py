ed7a82f3c7a930a8a2154871173e2e0a84279957
Paulius <Paulius@DESKTOP-JO1I1KK>
2023-07-25 23:21:10 +0300

    Added blocks loop

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ cell_number = 20
 
 height = cell_number * cell_size
 width = cell_number * cell_size
-framerate = 120
+framerate = 12
 
 pygame.init()
 
@@ -37,43 +37,68 @@ def end_game():
     pygame.quit()
     sys.exit()
 
+class snake:
+    def __init__(self, x_pos, y_pos, x_step, y_step, cell_size):
+        self.cell_size = cell_size
+        self.x_pos, self.y_pos = x_pos, y_pos
+        self.x_step, self.y_step = x_step, y_step
+
+    def update_speed(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                if self.y_step == -self.cell_size:
+                    end_game()
+                else:
+                    self.x_step, self.y_step = 0, self.cell_size
+                    # x_step = 0
+            elif event.key == pygame.K_UP:
+                if self.y_step == self.cell_size:
+                    end_game()
+                else:
+                    self.x_step, self.y_step = 0, -self.cell_size
+            elif event.key == pygame.K_RIGHT:
+                if self.x_step == -self.cell_size:
+                    end_game()
+                else:
+                    self.x_step, self.y_step = self.cell_size, 0
+            elif event.key == pygame.K_LEFT:
+                if self.x_step == self.cell_size:
+                    end_game()
+                else:
+                    self.x_step, self.y_step = -self.cell_size, 0
+
+        self.x_pos += self.x_step
+        self.y_pos += self.y_step
+        self.check_bounds(width, height)
+
+    def get_coordinates(self):
+        return self.x_pos, self.y_pos
+    
+    def get_speed(self):
+        return self.x_step, self.y_step
+
+    def get_rect_tuple(self):
+        return [self.x_pos, self.y_pos, self.cell_size, self.cell_size]
+
+    def check_bounds(self, height, width):
+        print(self.x_pos, self.y_pos)
+        if any([self.x_pos < 0, self.x_pos >= width, self.y_pos < 0, self.y_pos >= height]):
+            end_game()
+
+
+blocks = [snake(x_pos, y_pos, x_step, y_step, cell_size)]
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             end_game()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                if y_step == -cell_size:
-                    end_game()
-                else:
-                    x_step, y_step = 0, cell_size
-                    # x_step = 0
-            elif event.key == pygame.K_UP:
-                if y_step == cell_size:
-                    end_game()
-                else:
-                    x_step, y_step = 0, -cell_size
-            elif event.key == pygame.K_RIGHT:
-                if x_step == -cell_size:
-                    end_game()
-                else:
-                    x_step, y_step = cell_size, 0
-            elif event.key == pygame.K_LEFT:
-                if x_step == cell_size:
-                    end_game()
-                else:
-                    x_step, y_step = -cell_size, 0
-
-
-    x_pos += x_step
-    y_pos += y_step
-    if any([x_pos < 0, x_pos >= width, y_pos < 0, y_pos >= height]):
-        end_game()
-
     screen.fill(pygame.Color("grey"))
-    pygame.draw.rect(screen, pygame.Color("green"), (x_pos, y_pos, cell_size, cell_size))
-    pygame.time.wait(100)
-
+    
+    for block in blocks:
+        block.update_speed(event)
+        block.check_bounds(*screen.get_size())
+        pygame.draw.rect(screen, pygame.Color("green"), block.get_rect_tuple())
+    
 
 
 
