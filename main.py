f10cb67c8faee046920d3c24d4980d80e98aef19
Paulius <Paulius@DESKTOP-JO1I1KK>
2023-08-08 00:05:34 +0300

    Moved settings to class. Added saves setting and revert to last saved.

--- main.py
+++ main.py
@@ -17,23 +17,6 @@ def end_game():
     sys.exit()
 
 
-def read_settings():
-    """Reads settings json file and returns settings as dictionary.
-
-    Returns:
-        dict: Settings dictionary.
-    """
-    file_path = os.path.realpath(__file__)
-    sep = os.path.sep
-    file_path = file_path.split(sep)
-    file_path = file_path[:-1]
-    file_path.append("snake settings.json")
-    file_path = sep.join(file_path)
-    with open(file_path) as f:
-        settings = f.read()
-        return json.loads(settings)
-
-
 def direction(event, cell_size, x_step, y_step):
     """Updates snake movement direction by arrow button clicks.
 
@@ -53,22 +36,75 @@ def direction(event, cell_size, x_step, y_step):
             x_step, y_step = 0, cell_size
     elif event.key == pygame.K_LEFT:
         if x_step == cell_size:
-            x_step, y_step =  -1, -1
+            x_step, y_step = -1, -1
         else:
             x_step, y_step = -cell_size, 0
     elif event.key == pygame.K_UP:
         if y_step == cell_size:
-            x_step, y_step =  -1, -1
+            x_step, y_step = -1, -1
         else:
             x_step, y_step = 0, -cell_size
     elif event.key == pygame.K_RIGHT:
         if x_step == -cell_size:
-            x_step, y_step =  -1, -1
+            x_step, y_step = -1, -1
         else:
             x_step, y_step = cell_size, 0
 
     return x_step, y_step
 
+
+class Settings():
+    cell_number: int
+    cell_size: int
+    framerate: int
+
+    def __init__(self):
+        self.get_file_path()
+        self.read_settings()
+
+    def get_file_path(self):
+        file_path = os.path.realpath(__file__)
+        sep = os.path.sep
+        file_path = file_path.split(sep)
+        file_path = file_path[:-1]
+        file_path.append("snake settings.json")
+        self.file_path = sep.join(file_path)
+
+    def read_settings(self):
+        with open(self.file_path) as f:
+            settings = f.read()
+            self.settings = json.loads(settings)
+        for key, value in self.settings.items():
+            setattr(self, key, value)
+
+    def save_settings(self):
+        with open(self.file_path, 'w') as f:
+            json.dump(self.settings, f)
+
+    def update(self, attr, value):
+        self.settings[attr] = value
+        setattr(self, attr, value)
+
+    def update_numeric(self, attr, value):
+        try:
+            value = int(value)
+        except ValueError:
+            value = 0
+        self.update(attr, value)
+
+    @property
+    def height(self):
+        return self.cell_number * self.cell_size
+
+    @property
+    def width(self):
+        return self.cell_number * self.cell_size
+
+    def revert(self):
+        self.__init__()
+
+    def __str__(self):
+        return self.settings.__str__()
 
 def play_game():
     """Displays and updates game screen. Contains all game logic.
@@ -80,23 +116,21 @@ def play_game():
     Returns:
         int: Score when game is stopped.
     """
-    settings = read_settings()
+    settings = Settings()
 
-    cell_size = settings["cell_size"]
-    cell_number = settings["cell_number"]
-    framerate = settings["speed"]
-    height = cell_number * cell_size
-    width = cell_number * cell_size
-    x_step = cell_size
+    x_step = settings.cell_size
     y_step = 0
     score = 0
 
     clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((height, width + cell_size))
+    screen = pygame.display.set_mode((settings.width, settings.height + settings.cell_size))
 
-    food = pygame.Rect(randint(0, cell_number-1)*cell_size, randint(0, cell_number-1)*cell_size, cell_size, cell_size)
+    food = pygame.Rect(randint(0, settings.cell_number - 1) * settings.cell_size,
+                       randint(0, settings.cell_number - 1) * settings.cell_size, settings.cell_size,
+                       settings.cell_size)
 
-    blocks = [pygame.Rect(0+cell_size, 0, cell_size, cell_size),pygame.Rect(0, 0, cell_size, cell_size)]
+    blocks = [pygame.Rect(0 + settings.cell_size, 0, settings.cell_size, settings.cell_size),
+              pygame.Rect(0, 0, settings.cell_size, settings.cell_size)]
     font = pygame.font.SysFont("Arial", 20)
 
     while True:
@@ -105,77 +139,81 @@ def play_game():
                 show_main_menu()
 
             elif event.type == pygame.KEYDOWN:
-                x_step, y_step = direction(event, cell_size, x_step, y_step)
+                x_step, y_step = direction(event, settings.cell_size, x_step, y_step)
                 if x_step == y_step:
                     show_main_menu()
 
         screen.fill(pygame.Color("grey"))
 
         for i, block in enumerate(reversed(blocks)):
-            if i == len(blocks)-1:
+            if i == len(blocks) - 1:
                 block.move_ip(x_step, y_step)
                 if pygame.Rect.colliderect(block, food):
-                    food.update(randint(0, cell_number-1)*cell_size, randint(0, cell_number-1)*cell_size, cell_size, cell_size)
+                    food.update(randint(0, settings.cell_number - 1) * settings.cell_size,
+                                randint(0, settings.cell_number - 1) * settings.cell_size, settings.cell_size,
+                                settings.cell_size)
 
                     score += 1
 
                     while food.collidelist(blocks) > -1:
-                        food.update(randint(0, cell_number - 1) * cell_size, randint(0, cell_number - 1) * cell_size, cell_size, cell_size)
+                        food.update(randint(0, settings.cell_number - 1) * settings.cell_size,
+                                    randint(0, settings.cell_number - 1) * settings.cell_size, settings.cell_size,
+                                    settings.cell_size)
 
                     blocks.append(blocks[-1].copy())
 
                 elif block.collidelist(blocks[1:]) > -1:
                     show_main_menu()
             else:
-                block.move_ip(blocks[-i-2].left-block.left, blocks[-i-2].top-block.top)
+                block.move_ip(blocks[-i - 2].left - block.left, blocks[-i - 2].top - block.top)
 
-            if any([block.left < 0, block.right > width, block.top < 0, block.bottom > height]):
+            if any([block.left < 0, block.right > settings.width, block.top < 0, block.bottom > settings.height]):
                 show_main_menu()
 
             pygame.draw.rect(screen, pygame.Color("brown"), block)
             pygame.draw.rect(screen, pygame.Color("black"), block, width=2)
 
-        score_rect = pygame.draw.rect(screen, pygame.Color("orange"), (0, height, width, cell_size))
+        score_rect = pygame.draw.rect(screen, pygame.Color("orange"),
+                                      (0, settings.height, settings.width, settings.cell_size))
 
         font_block = font.render(score.__str__(), True, pygame.Color("black"))
         screen.blit(font_block, score_rect)
 
         pygame.draw.rect(screen, pygame.Color("red"), food)
 
-        pygame.draw.rect(screen, pygame.Color("orange"), (height, 0, width, cell_size))
+        pygame.draw.rect(screen, pygame.Color("orange"), (settings.height, 0, settings.width, settings.cell_size))
 
         pygame.display.update()
-        clock.tick(framerate)
+        clock.tick(settings.framerate)
 
 
 def show_settings():
     # https: // www.geeksforgeeks.org / create - settings - menu - in -python - pygame /
-    settings = read_settings()
-    height = settings["cell_number"] * settings["cell_size"]
-    width = settings["cell_number"] * settings["cell_size"]
-    framerate = settings["speed"]
+    settings = Settings()
 
-    screen = pygame.display.set_mode((width, height))
+    screen = pygame.display.set_mode((settings.width, settings.height))
 
-    settings_menu = pm.Menu(title="Settings", width=width, height=height)
+    settings_menu = pm.Menu(title="Settings", width=settings.width, height=settings.height)
+    settings_menu.add.text_input(title="Speed: ", default=str(settings.framerate), onchange=lambda value: settings.update_numeric("framerate", value))
 
-    settings_menu.add.text_input(title="Speed: ", default=str(framerate))
-    settings_menu.add.text_input(title="cell size: ", default=str(settings["cell_number"]))
-    settings_menu.add.text_input(title="Number of cells: ", default=str(settings["cell_size"]))
+    settings_menu.add.text_input(title="cell size: ", default=str(settings.cell_size), onchange=lambda value: settings.update_numeric("cell_size", value))
+    settings_menu.add.text_input(title="Number of cells: ", default=str(settings.cell_number), onchange=lambda value: settings.update_numeric("cell_number", value))
 
     settings_menu.add.label("")
-    settings_menu.add.button(title="SAVE", font_color=(0, 0, 0), action=play_game)
+    settings_menu.add.button(title="SAVE", font_color=(0, 0, 0), action=settings.save_settings)
+    settings_menu.add.button(title="Revert to last saved", font_color=(0, 0, 0), action=show_settings)
     settings_menu.add.button(title="Cancel", font_color=(0, 0, 0), action=show_main_menu)
 
     settings_menu.mainloop(screen)
 
+
 def show_main_menu():
-    settings = read_settings()
-    screen = pygame.display.set_mode((settings["cell_number"]*settings["cell_size"], settings["cell_number"]*settings["cell_size"]))
+    settings = Settings()
+    screen = pygame.display.set_mode((settings.width, settings.height))
 
     main_menu = pm.Menu(title="Snake RPG"
-                        , width=settings["cell_number"]*settings["cell_size"]
-                        , height=settings["cell_number"]*settings["cell_size"])
+                        , width=settings.width
+                        , height=settings.height)
 
     main_menu.add.button(title="PLAY", font_color=(0, 0, 0), action=play_game)
 
@@ -183,6 +221,7 @@ def show_main_menu():
     main_menu.add.button(title="Quit", font_color=(0, 0, 0), action=pm.events.EXIT)
 
     main_menu.mainloop(screen)
+
 
 # show_screen = "SETTINGS"
 pygame.init()
